af96947d1bf7ef4bcd4da06fde6df42e549b330e
robin-dela <delaporte.ro@gmail.com>
2015-12-03 16:11:36 +0100

    add hotspot animation

--- app/objects/Hotspot.js
+++ app/objects/Hotspot.js
@@ -3,13 +3,11 @@ import PlanView from './../views/PlantView'
 export default class Hotspot {
   constructor(datas) {
 
-
-
     var element = document.createElement( 'div' );
     element.innerHTML = "<svg width='100' height='100'><circle cx='50' cy='50' r='40' stroke='green' stroke-width='4' fill='yellow' /></svg>"
 		element.id = datas.id;
 		element.className = 'hotspot';
-		// element.style.opacity = 0.5;
+		//element.style.opacity = 0.5;
     element.style.width = "20px";
     element.style.height = "20px";
 
