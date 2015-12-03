0baa4ea35ff7f652e9268327737c61fd4a3155c5
robin-dela <delaporte.ro@gmail.com>
2015-12-03 17:21:15 +0100

    fix svg position

--- app/objects/Hotspot.js
+++ app/objects/Hotspot.js
@@ -4,20 +4,18 @@ export default class Hotspot {
   constructor(datas) {
 
     var element = document.createElement( 'div' );
-    element.innerHTML = "<svg width='100' height='100'><circle cx='50' cy='50' r='40' stroke='green' stroke-width='4' fill='yellow' /></svg>"
+    element.innerHTML = "<svg><circle cx='5' cy='5' r='10' fill='white'/></svg>"
 		element.id = datas.id;
 		element.className = 'hotspot';
 		//element.style.opacity = 0.5;
-    element.style.width = "20px";
-    element.style.height = "20px";
+    element.style.width = "10px";
+    element.style.height = "10px";
 
 		this.objectCss = new THREE.CSS3DObject( element );
 		this.objectCss.position.x = 160 *Math.cos(datas.position.angle);
 		this.objectCss.position.y = datas.position.y;
 		this.objectCss.position.z = 160 *Math.sin(datas.position.angle);
     this.objectCss.element.parent = this.objectCss;
-
-
 
     this.objectCss.element.onclick = function() {
       let plantView = new PlanView({
