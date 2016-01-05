3f367997962da2947c0faf9cc5a65bcbfa2bb2a6
robin-dela <delaporte.ro@gmail.com>
2016-01-05 16:43:49 +0100

    add jojo

--- app/views/PlantView.js
+++ app/views/PlantView.js
@@ -9,21 +9,24 @@ export default class PlantView {
     this.datas = options.datas;
     this.template = template;
   }
+
   show() {
     TweenLite.to(this.el,.3,{
       left:'0%'
     })
   }
+
   hide () {
     TweenLite.to(this.el,.3,{
       left:'100%'
     })
-    window.removeEventListener('deviceorientation',  this._onDeviceOrientation)
-
+    window.removeEventListener('deviceorientation',  this._onDeviceOrientation);
   }
+
   update(datas) {
     this.datas = datas;
   }
+
   addToWishList() {
     WishList.add(this.datas);
     this.addBtn.classList.add('animate');
@@ -32,12 +35,13 @@ export default class PlantView {
         this.hide();
     }.bind(this), 1000);
   }
+
   render() {
     let html = Mustache.to_html(this.template, this.datas);
     this.el.innerHTML = html;
     this.rendered();
-
   }
+
   rendered() {
     this.closeBtn = document.querySelector('.back');
     this.addBtn = document.querySelector('.button');
@@ -55,10 +59,9 @@ export default class PlantView {
     window.addEventListener('deviceorientation',  this._onDeviceOrientation);
     this.translateX = 0 ;
     this.translateY = 0 ;
-
   }
-  onDeviceOrientation(e) {
 
+  onDeviceOrientation(e) {
     this.translateX += (e.gamma - this.translateX );
     this.translateY += (e.beta - this.translateY );
 
@@ -67,7 +70,5 @@ export default class PlantView {
         transform: 'translateX('+this.translateX/this.images[i].random+'px) translateY('+this.translateY/this.images[i].random+'px)'
       })
     }
-
-
   }
 }
