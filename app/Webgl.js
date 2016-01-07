0d5ca7384ae5e22e526cc8a32e9a7ba54088e322
robin-dela <delaporte.ro@gmail.com>
2016-01-07 10:46:54 +0100

    push for fucking jojo

--- app/Webgl.js
+++ app/Webgl.js
@@ -29,7 +29,6 @@ export default class Webgl {
 
     this.camera = new THREE.PerspectiveCamera(45, width / height, 0.001, 1000);
 
-
     this.camera.position.set(0,0,1);
 
     this.renderer = new THREE.WebGLRenderer();
@@ -43,18 +42,15 @@ export default class Webgl {
 		this.rendererCss.domElement.style.top = 0;
 
     this.buildHotspot();
-
+    this.initMenu();
 
     this.composer = null;
     this.initPostprocessing();
-
 
     this.cylinder = new Cylinder();
     this.cylinder.add(this.scene);
 
     window.webgl = this;
-
-
 
 
     // if(isTouch)
@@ -72,26 +68,38 @@ export default class Webgl {
     if (!this.params.usePostprocessing) { return; }
 
   }
-  buildHotspot() {
 
+  buildHotspot() {
     for (var i = 0; i < this.datas.hotspots.length; i++) {
         let hotspot = new Hotspot(this.datas.hotspots[i]);
         hotspot.add(this.sceneCss);
         this.hotspots.push(hotspot)
     }
+  }
 
+  initMenu(){
+    this.closeBtn = document.querySelector('.close');
+    this.showBtn = document.querySelector('.left');
+    this.menu = document.querySelector('.full-menu');
+
+    this.showBtn.onclick =()=> {
+      this.menu.classList.toggle('active');
+    }
+    this.closeBtn.onclick =()=> {
+      this.menu.classList.toggle('active');
+    }
   }
 
   resize(width, height) {
     if (this.composer) {
       this.composer.setSize(width, height);
     }
-
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
     this.rendererCss.setSize(width, height);
     this.renderer.setSize(width, height);
   }
+
   render() {
     if (this.params.usePostprocessing) {
       console.warn('WebGL - No effect composer set.');
@@ -102,11 +110,8 @@ export default class Webgl {
       for (var i = 0; i < this.hotspots.length; i++) {
           this.hotspots[i].check();
           this.hotspots[i].objectCss.lookAt( this.camera.position );
+          this.hotspots[i].objectCss2.lookAt( this.camera.position );
       }
-
-
-
-
     }
     this.cylinder.update();
     this.controls.update();
