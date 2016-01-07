7e4292b808d8bc152f4c40f801ed15e8c75670f2
MACHADO Jordan <jmachado@vaioz-alm.intra.gobelins.fr>
2016-01-07 17:25:48 +0100

    Madj

--- app/Webgl.js
+++ app/Webgl.js
@@ -54,9 +54,9 @@ export default class Webgl {
 
 
 
-    if(isTouch)
-      this.controls = new DeviceOrientationControls( this.camera );
-    else
+    // if(isTouch)
+    //   this.controls = new DeviceOrientationControls( this.camera );
+    // else
 
       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
       this.controls.minPolarAngle = Math.PI/180*90;
---
+++ ios/sisley/sisley/Annotation.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+#import <MapKit/MKAnnotation.h>
+
+@interface Annotation : NSObject <MKAnnotation> {
+    
+    CLLocationCoordinate2D coordinate;
+    NSString *title;
+    NSString *subtitle;
+    
+}
+
+@property(nonatomic, assign) CLLocationCoordinate2D coordinate;
+@property(nonatomic, copy) NSString *title;
+@property(nonatomic, copy) NSString *subtitle;
+
+@end
